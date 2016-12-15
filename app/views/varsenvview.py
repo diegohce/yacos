9dd3894e3adefaf36036bcdb1ea64878ec95a32e
Diego Cena <diego.cena@gmail.com>
2016-12-14 23:49:13 -0300

    dev commit

--- app/views/varsenvview.py
+++ app/views/varsenvview.py
@@ -35,17 +35,26 @@ class VarsEnvView(MethodView):
 		gglobals    = Global.select().order_by(Global.name)
 
 		templatebody = template.body
-		print templatebody
-		vars_from_template = self.__parse_template(unicode(templatebody))
+		vars_from_template = self.__parse_template(templatebody)
 
 
 		try:
-			variables = Variable.select().where(Variable.template == template , Variable.env == environment)
+			variables = (Variable.select()
+                         .where(Variable.template == template, 
+                                Variable.env == environment) )
 		except Variable.DoesNotExist:
-			variables = None	
-		
+			variables = []
+
+		variables_d = dict.fromkeys([ v.strip() for v in vars_from_template ], '')
+
+		for v in variables:
+			variables_d[ v.name ] = v.value
+
+		resolved_globals = self.__resolve_globals(environment, variables_d)
+		variables_d.update( resolved_globals )
 
 		return flask.render_template('variables.html',
+					variables_d=variables_d,
 					vars_from_template=vars_from_template,
 					variables=variables,
 					gglobals=gglobals,
@@ -59,8 +68,19 @@ class VarsEnvView(MethodView):
 
 	def __parse_template(self, body):
 		m = re.findall('\{\{([a-z,A-Z,_,.,0-9, ]*)\}\}', body)
-		print m
+#		print m
 		return m
+
+	def __resolve_globals(self, environment, variables_d):
+	
+		gg_d = {}
+		for var_name in filter(lambda v: v.startswith('global.'), variables_d):
+			try:
+				gg = Global.get(Global.name == var_name[len('global.'):], Global.env == environment)
+				gg_d[ var_name ] = gg.value
+			except Global.DoesNotExist, e:
+				app.logger.debug( repr(e) )
+		return gg_d			
 
 ##
 #
